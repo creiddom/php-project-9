e1ad2cca15713f58fe4c2da56c1eb4d0f6d13652
Elizaveta Galimova <egalimova@marfa-tech.com>
2026-04-30 10:18:01 +0500

    template

--- public/index.php
+++ public/index.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+$templatesPath = dirname(__DIR__) . '/templates';
+$renderer = new PhpRenderer($templatesPath, [], 'layout.php');
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response): Response {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+$app->get('/', function (Request $request, Response $response) use ($renderer): Response {
+    return $renderer->render($response, 'home.php', [
+        'title' => 'Анализатор страниц',
+        'flash' => [],
+    ]);
 });
 
 $app->run();
