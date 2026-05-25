f037d96623f7ce16af3b348b44dfb020eeb45a1c
Elizaveta Galimova <egalimova@marfa-tech.com>
2026-05-25 14:57:00 +0500

    require_once

--- public/index.php
+++ public/index.php
@@ -12,14 +12,14 @@ session_start();
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->useAutowiring(true);
 
-$dependencies = require dirname(__DIR__) . '/config/dependencies.php';
+$dependencies = require_once dirname(__DIR__) . '/config/dependencies.php';
 $dependencies($containerBuilder);
 
 $container = $containerBuilder->build();
 
 $app = $container->get(App::class);
 
-$routes = require dirname(__DIR__) . '/config/routes.php';
+$routes = require_once dirname(__DIR__) . '/config/routes.php';
 $routes($app, $container);
 
 $app->run();
